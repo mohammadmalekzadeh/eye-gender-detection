210401ea8323ed76999ee91d08ba5bbe6a291728
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-07-26 15:40:49 +0330

    Update save_load.py

--- src/model/save_load.py
+++ src/model/save_load.py
@@ -1,8 +1,11 @@
 import joblib
 from src.utlis import BASE_DIR
 
-def save(name, model):
-    joblib.dump(model, BASE_DIR+'/models/'+name+'.pkl')
+def save(name, model, best=False):
+    if best:
+        joblib.dump(model, BASE_DIR+'/outputs/models/best_model_'+name+'.pkl')
+    else:
+        joblib.dump(model, BASE_DIR+'/models/'+name+'.pkl')
 
 def load(full_path):
     return joblib.load(full_path)
